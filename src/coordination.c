0a8aa113532a0fc248b8a5513e858c81de160d22
Piotr Wróbel <pwrobel5@gmail.com>
2021-02-08 14:46:52 +0100

    corrected coordination

--- src/coordination.c
+++ src/coordination.c
@@ -91,18 +91,18 @@ void save_data_from_array(struct oxygen_coord_info* array, FILE* output_file)
     }
 }
 
-void save_last_step_data(struct oxygen_coord_info** carbonate_data, struct oxygen_coord_info*** tfsi_data, struct system_info* system_info, FILE* carbonates_output, FILE* tfsi_output)
+void save_last_step_data(struct oxygen_coord_info** solvent_data, struct oxygen_coord_info*** anion_data, struct system_info* system_info, FILE* solvent_output, FILE* anion_output)
 {
-    for(int i = 0; i < system_info->carbonate_molecules_number; i++)
+    for(int i = 0; i < system_info->solvent_molecules_number; i++)
     {
-        save_data_from_array(carbonate_data[i], carbonates_output);
+        save_data_from_array(solvent_data[i], solvent_output);
     }
 
-    for(int i = 0; i < system_info->tfsi_molecules_number; i++)
+    for(int i = 0; i < system_info->anions_number; i++)
     {
-        for(int j = 0; j < TRACKED_O_ATOMS_TFSI; j++)
+        for(int j = 0; j < TRACKED_O_ATOMS_ANION; j++)
         {
-            save_data_from_array(tfsi_data[i][j], tfsi_output);
+            save_data_from_array(anion_data[i][j], anion_output);
         }
     }
 }
@@ -150,89 +150,89 @@ void calculate_coord_times(int molecules_number, int** current_coordination, int
     }
 }
 
-void calculate_coord_times_tfsi(struct system_info* system_info, struct tfsi_data* tfsi_data)
+void calculate_coord_times_anion(struct system_info* system_info, struct anion_data* anion_data)
 {
-    for(int i = 0; i < system_info->tfsi_molecules_number; i++)
+    for(int i = 0; i < system_info->anions_number; i++)
     {
-        calculate_coord_times(TRACKED_O_ATOMS_TFSI, tfsi_data->current_tfsi_coordination[i], tfsi_data->last_tfsi_coordination[i], tfsi_data->tfsi_coordination_info[i], tfsi_data->tfsi_output);
+        calculate_coord_times(TRACKED_O_ATOMS_ANION, anion_data->current_anion_coordination[i], anion_data->last_anion_coordination[i], anion_data->anion_coordination_info[i], anion_data->anion_output);
     }
 }
 
-void calculate_coord_times_carbonates(struct system_info* system_info, struct carbonate_data* carbonate_data)
+void calculate_coord_times_solvent(struct system_info* system_info, struct solvent_data* solvent_data)
 {
-    calculate_coord_times(system_info->carbonate_molecules_number, carbonate_data->current_carbonate_coordination, carbonate_data->last_carbonate_coordination,
-                          carbonate_data->carbonate_coordination_info, carbonate_data->carbonate_output);
+    calculate_coord_times(system_info->solvent_molecules_number, solvent_data->current_solvent_coordination, solvent_data->last_solvent_coordination,
+                          solvent_data->solvent_coordination_info, solvent_data->solvent_output);
 }
 
 struct metal_coord_info get_coordination_info(int metal_index, struct vector metal_position, struct coordination_input* coordination_input)
 {
     struct metal_coord_info result;
-    result.carbonate_molecules = 0;
-    result.tfsi_molecules = 0;
-    result.tfsi_oxygens = 0;
+    result.solvent_molecules = 0;
+    result.anion_molecules = 0;
+    result.anion_oxygens = 0;
     result.coordination_number = 0;
 
     struct program_configuration* program_configuration = coordination_input->program_configuration;
-    int carbonate_history_index = 0;
-    int tfsi_atoms_history_index = 0;
-    int tfsi_history_index = 0;
-    int carbonate_oxygen_index = 0;
+    int solvent_history_index = 0;
+    int anion_atoms_history_index = 0;
+    int anion_history_index = 0;
+    int solvent_oxygen_index = 0;
 
     int compound_index = 0;
-    for(int i = 0; i < coordination_input->system_info->carbonate_types_number; i++)
+    for(int i = 0; i < coordination_input->system_info->solvent_types_number; i++)
     {
-        compound_index = get_next_carbonate_index(compound_index, coordination_input->system_info->compounds, coordination_input->system_info->compounds_number);
+        compound_index = get_next_solvent_index(compound_index, coordination_input->system_info->compounds, coordination_input->system_info->compounds_number);
         for(int j = 0; j < coordination_input->system_info->compounds[compound_index].quantity; j++)
         {
-            if(detect_coordination(metal_position, coordination_input->carbonate_oxygens[i][j], program_configuration->carbonate_threshold, program_configuration->box_size) == 1)
+            if(detect_coordination(metal_position, coordination_input->solvent_oxygens[i][j], program_configuration->solvent_threshold, program_configuration->box_size) == 1)
             {
                 result.coordination_number++;
-                result.carbonate_molecules++;
+                result.solvent_molecules++;
 
-                if(program_configuration->calculate_carbonate_residence == 1)
+                if(program_configuration->calculate_solvent_residence == 1)
                 {
-                    coordination_input->carbonate_coordination_histories[i][coordination_input->step_number][metal_index][carbonate_history_index] = j;
-                    carbonate_history_index++;
+                    coordination_input->solvent_coordination_histories[i][coordination_input->step_number][metal_index][solvent_history_index] = j;
+                    solvent_history_index++;
                 }
                 
-                mark_coordination(metal_index, coordination_input->current_carbon_coordination[carbonate_oxygen_index]);
+                mark_coordination(metal_index, coordination_input->current_solvent_coordination[solvent_oxygen_index]);
             }
             
-            carbonate_oxygen_index++;
+            solvent_oxygen_index++;
         }
 
-        carbonate_history_index = 0;
+        solvent_history_index = 0;
     }
 
-    for(int i = 0; i < coordination_input->system_info->tfsi_molecules_number; i++)
+    for(int i = 0; i < coordination_input->system_info->anions_number; i++)
     {
-        int is_metal_coordinated_by_tfsi = 0;
-        for(int j = 0; j < TRACKED_O_ATOMS_TFSI; j++)
+        int is_metal_coordinated_by_anion = 0;
+        for(int j = 0; j < TRACKED_O_ATOMS_ANION; j++)
         {
-            if(detect_coordination(metal_position, coordination_input->tfsi_oxygens[i][j], program_configuration->tfsi_threshold, program_configuration->box_size) == 1)
+            if(detect_coordination(metal_position, coordination_input->anion_oxygens[i][j], program_configuration->anion_threshold, program_configuration->box_size) == 1)
             {
                 result.coordination_number++;
-                result.tfsi_oxygens++;
-                is_metal_coordinated_by_tfsi = 1;
+                result.anion_oxygens++;
+                is_metal_coordinated_by_anion = 1;
 
-                if(program_configuration->calculate_tfsi_residence == 1)
+                if(program_configuration->calculate_anion_residence == 1)
                 {
-                    coordination_input->tfsi_atoms_coordination_history[coordination_input->step_number][metal_index][tfsi_atoms_history_index] = i * TRACKED_O_ATOMS_TFSI + j;
-                    tfsi_atoms_history_index++;
+                    coordination_input->anion_atoms_coordination_history[coordination_input->step_number][metal_index][anion_atoms_history_index] = i * TRACKED_O_ATOMS_ANION + j;
+                    anion_atoms_history_index++;
                 }
 
-                mark_coordination(metal_index, coordination_input->current_tfsi_coordination[i][j]);
+                mark_coordination(metal_index, coordination_input->current_anion_coordination[i][j]);
             }
         }
 
-        if(is_metal_coordinated_by_tfsi == 1)
+        if(is_metal_coordinated_by_anion == 1)
         {
-            result.tfsi_molecules++;
+            result.anion_molecules++;
 
-            if(program_configuration->calculate_tfsi_residence == 1)
+            if(program_configuration->calculate_anion_residence == 1)
             {
-                coordination_input->tfsi_coordination_history[coordination_input->step_number][metal_index][tfsi_history_index] = i;
-                tfsi_history_index++;
+                coordination_input->anion_coordination_history[coordination_input->step_number][metal_index][anion_history_index] = i;
+                anion_history_index++;
             }
         }
     }
